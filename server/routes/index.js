0a5372be6355ca4377446e75e05fb87c895fe6b4
creme332 <65414576+creme332@users.noreply.github.com>
2023-10-16 21:46:59 +0400

    define all routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({ title: 'Express' });
-});
+// Require controller modules.
+const item_controller = require("../controllers/bookController");
+const category_controller = require("../controllers/authorController");
+
+router.get("/categories", category_controller.category_list);
+router.get("/category/:id", category_controller.category_detail);
+router.post("/category/create", category_controller.category_create_post);
+router.post("/categories/:id/update", category_controller.category_update_post);
+router.post("/categories/:id/delete", category_controller.category_delete_post);
+
+router.get("/items", item_controller.item_list);
+router.get("/item/:id", item_controller.item_detail);
+router.post("/item/create", item_controller.item_create_post);
+router.post("/item/:id/update", item_controller.item_update_post);
+router.post("/item/:id/delete", item_controller.item_delete_post);
 
 module.exports = router;
