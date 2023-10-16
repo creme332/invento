0bbb60981bac36cd446d38e52adfc7544f4395ab
creme332 <65414576+creme332@users.noreply.github.com>
2023-10-16 21:46:21 +0400

    add item controller

---
+++ server/controllers/itemController.js
@@ -0,0 +1,94 @@
+const { body, validationResult } = require("express-validator");
+const asyncHandler = require("express-async-handler");
+const Item = require("../models/item");
+
+// Display list of all Categories.
+exports.item_list = asyncHandler(async (req, res, next) => {
+  const allItems = await Item.find().sort({ name: 1 }).exec();
+  res.json(allItems);
+});
+
+// Display one Item
+exports.item_detail = asyncHandler(async (req, res, next) => {
+  // Get details of item and all their books (in parallel)
+  const item = Item.findById(req.params.id).exec();
+
+  if (item === null) {
+    // No results.
+    res.redirect("/items");
+  }
+
+  res.json(item);
+});
+
+// Handle Item create on POST.
+exports.item_create_post = [
+  // Validate and sanitize fields.
+  body("name")
+    .trim()
+    .isLength({ min: 3 })
+    .escape()
+    .withMessage("Name must have at least 3 characters.")
+    .isAlphanumeric()
+    .withMessage("Name has non-alphanumeric characters."),
+  body("description")
+    .trim()
+    .isLength({ min: 3 })
+    .escape()
+    .withMessage("Description must have at least 3 characters.")
+    .isAlphanumeric()
+    .withMessage("Description has non-alphanumeric characters."),
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create Item object with escaped and trimmed data
+    const itemDict = {
+      name: req.body.name,
+      description: req.body.description,
+      status: req.body.status,
+      stock: req.body.stock,
+      image: req.body.image,
+      price: req.body.price,
+      category: req.body.category,
+    };
+    if (itemDict.image) {
+      itemDict.image = req.body.image;
+    }
+    const item = new Item(itemDict);
+
+    if (!errors.isEmpty()) {
+      res.json({
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+
+      // Save item.
+      await item.save();
+      // Redirect to new item record.
+      res.redirect(item.url);
+    }
+  }),
+];
+
+// Handle Item delete on POST.
+// TODO: Add validation
+exports.item_delete_post = asyncHandler(async (req, res, next) => {
+  const item = Item.findById(req.params.id).exec();
+  await Item.findByIdAndRemove(req.body.itemid);
+  res.redirect("/items");
+});
+
+// Handle Item update on POST.
+exports.item_update_post = asyncHandler(async (req, res, next) => {
+  // Create a Item object with escaped/trimmed data and old id.
+  const item = new Item({
+    name: req.body.name,
+    description: req.body.description,
+  });
+
+  await Item.findByIdAndUpdate(req.params.id, item, {});
+  res.redirect(item.url);
+});
