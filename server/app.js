efc7bf1dcfba6b78035b217d38f305be5eabd54b
creme332 <65414576+creme332@users.noreply.github.com>
2023-10-20 14:14:13 +0400

    add cors

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-
+const cors = require("cors");
 const indexRouter = require("./routes/index");
 
 const app = express();
@@ -21,6 +21,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use("/", indexRouter);
 
