62df10643ca50690196b5e7681c1e695c5e25e15
creme332 <65414576+creme332@users.noreply.github.com>
2023-10-21 16:16:50 +0400

    refactor category_create_post and category_delete_post.
    add correct status code when error occurs.

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -33,45 +33,51 @@ exports.category_create_post = [
     .escape()
     .withMessage("Name must have at least 3 characters.")
     .isAlphanumeric()
-    .withMessage("Name has non-alphanumeric characters."),
+    .withMessage("Name must not contain non-alphanumeric characters."),
   body("description")
     .trim()
     .isLength({ min: 3 })
     .escape()
     .withMessage("Description must have at least 3 characters.")
     .isAlphanumeric()
-    .withMessage("Description has non-alphanumeric characters."),
+    .withMessage("Description must not non-alphanumeric characters."),
   asyncHandler(async (req, res, next) => {
     // Extract the validation errors from a request.
     const errors = validationResult(req);
-    console.log("received post request");
+
+    // Check for errors in received data. If so, send error messages
+    if (!errors.isEmpty()) {
+      res.writeHead(400, `${JSON.stringify(errors.array().map((e) => e.msg))}`);
+      return res.send();
+    }
+
+    // Check if category name is unique
+    const existingCategory = Category.find({ name: req.body.name }).exec();
+    if (existingCategory) {
+      res.writeHead(403, `Category ${req.body.name} already exists.`);
+      return res.send();
+    }
+
     // Create Category object with escaped and trimmed data
     const category = new Category({
       name: req.body.name,
       description: req.body.description,
     });
 
-    if (!errors.isEmpty()) {
-      res.writeHead(440, `${JSON.stringify()(errors.array())}`);
+    // Save to Mongo
+    try {
+      await category.save();
+      return res.send("success");
+    } catch (error) {
+      res.writeHead(500, `${error}`);
       res.send();
-    } else {
-      // Data from form is valid.
-      // TODO: Check if category name is unique
-      try {
-        // Save category.
-        await category.save();
-        // Send a valid response
-        res.json(category.url);
-      } catch (error) {
-        res.writeHead(440, `${error}`);
-        res.send();
-      }
     }
   }),
 ];
 
 // Handle Category delete on POST.
 exports.category_delete_post = asyncHandler(async (req, res, next) => {
+  // fetch category and items having that category
   const [category, items] = await Promise.all([
     Category.findById(req.params.id).exec(),
     Item.find({ category: req.params.id }).exec(),
@@ -79,30 +85,38 @@ exports.category_delete_post = asyncHandler(async (req, res, next) => {
 
   // check if category exists
   if (!category) {
-    const err = new Error("Category not found");
-    err.status = 404;
-    return next(err);
-  }
-
-  if (items.length > 0) {
-    // prevent deletion of a category if an item uses that category
-    res.writeHead(440, "Cannot delete a category that is used by an item.", {
+    res.writeHead(400, "Category does not exist", {
       "content-type": "application/json",
     });
+    return res.send();
+  }
+
+  // prevent deletion of a category if an item uses that category
+  if (items.length > 0) {
+    res.writeHead(
+      403,
+      `Cannot delete a category that is used by the following items: ${JSON.stringify(
+        items.map((e) => e.name)
+      )} `,
+      {
+        "content-type": "application/json",
+      }
+    );
+    return res.send();
+  }
+
+  try {
+    // category exists and is unused so delete it
+    await Category.findByIdAndRemove(req.params.id);
+    return res.send("success");
+  } catch (err) {
+    res.writeHead(500, `${error}`);
     res.send();
-    // res.send({ error: , items: items });
-  } else {
-    try {
-      // category exists and is unused so delete it
-      await Category.findByIdAndRemove(req.params.id);
-      res.redirect("/categories");
-    } catch (err) {
-      console.log(err);
-    }
   }
 });
 
 // Handle Category update on POST.
+// TODO: Add validation
 exports.category_update_post = asyncHandler(async (req, res, next) => {
   // Create a Category object with escaped/trimmed data and old id.
   const category = new Category({
