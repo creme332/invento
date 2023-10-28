366c78bcaf0b33c1a5f49732eb908580c0039b3f
creme332 <65414576+creme332@users.noreply.github.com>
2023-10-28 14:41:06 +0400

    add meta description

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -34,6 +34,10 @@ export default function App({ Component, pageProps }: any) {
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏨</text></svg>"
         />
         <meta
+          name="description"
+          content="A basic inventory management app with search capabilities built with MERN stack."
+        />
+        <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
