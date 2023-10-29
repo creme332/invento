4c4e2b0a2353eccaf76c46a479d0f8ef06b950a2
creme332 <65414576+creme332@users.noreply.github.com>
2023-10-29 18:49:13 +0400

    enable zooming

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -37,10 +37,7 @@ export default function App({ Component, pageProps }: any) {
           name="description"
           content="A basic inventory management app with search capabilities built with MERN stack."
         />
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Header />
       <Modal
